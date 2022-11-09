91041a440b4f0de1f67fcfa72f787a2330a6bc48
Stephen Gerald <stephenokpala11@gmail.com>
2022-11-09 08:31:37 -0800

    new changes

--- get_help.c
+++ get_help.c
@@ -19,4 +19,15 @@ int get_help(data_shell *datash)
 		aux_help();
 	else if (_strcmp(datash->args[1], "exit") == 0)
 		aux_help_exit();
+	else if (_strcmp(datash->args[1], "cd") == 0)
+		aux_help_cd();
+	else if (_strcmp(datash->args[1], "alias") == 0)
+		aux_help_alias();
+	else
+
+		write(STDERR_FILENO, datash->args[0],
+		      _strlen(datash->args[0]));
+
+	datash->status = 0;
+	return (1);
 }
