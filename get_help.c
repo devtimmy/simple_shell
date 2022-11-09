280172c8508a07fdd127114055c394736cf93f64
Stephen Gerald <stephenokpala11@gmail.com>
2022-11-09 08:27:08 -0800

    new changes

---
+++ get_help.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *get_help - function that retrieves help messages according builtin
+ * @datash: data structure (args and input)
+ * Return: Return 0
+ */
+int get_help(data_shell *datash)
+{
+	if (datash->args[1] == 0)
+		aux_help_general();
+	else if (_strcmp(datash->args[1], "setenv") == 0)
+		aux_help_setenv();
+	else if (_strcmp(datash->args[1], "env") == 0)
+		aux_help_env();
+	else if (_strcmp(datash->args[1], "unsetenv") == 0)
+		aux_help_unsetenv();
+	else if (_strcmp(datash->args[1], "help") == 0)
+		aux_help();
+	else if (_strcmp(datash->args[1], "exit") == 0)
+		aux_help_exit();
+}
