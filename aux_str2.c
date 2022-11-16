020cba8c603595e43d97016977deb53ee33b9743
Fatoye Timileyin <phartoyetimileyin@gmail.com>
2022-11-16 12:37:33 -0800

    commit changes

--- aux_str2.c
+++ aux_str2.c
@@ -30,7 +30,7 @@ int _strlen(const char *s)
 
 	for (len = 0; s[len] != 0; len++)
 	{
-	
+
 	}
 	return (len);
 }
--- exit_shell.c
+++ exit_shell.c
@@ -2,6 +2,7 @@
 
 /**
  * exit_shell - exits the shell
+ *
  * @datash: data relevant (status and args)
  * Return: 0 on success.
  */
@@ -18,11 +19,13 @@ int exit_shell(data_shell *datash)
 		is_digit = _isdigit(datash->args[1]);
 		str_len = _strlen(datash->args[1]);
 		big_number = ustatus > (unsigned int)INT_MAX;
-												if (!is_digit || str_len > 10 || big_number)
-												{
-			get_error(datash, 2);								datash->status = 2;
-			return (1);								}
-												datash->status = (ustatus % 256);
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			get_error(datash, 2);
+			datash->status = 2;
+			return (1);
+		}
+		datash->status = (ustatus % 256);
 	}
 	return (0);
 }
